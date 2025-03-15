2b0762e72f0f86b8dfa41bcde1b87bab7b338dc0
hervasa2 <david.hervas@tum.de>
2025-03-15 18:46:47 +0100

    update methods for geometry perturbations

--- src/Characterize.jl
+++ src/Characterize.jl
@@ -77,7 +77,7 @@ function characterize!(det::DetectorDesign{T}, imp_model::AbstractImpurityDensit
             Vdep = T(to_internal_units(estimate_depletion_voltage(sim, check_for_depletion = false, verbose = verbose)))
             det.Vdep = Vdep
             if verbose @info "Simulating at Vdep + 500V...\n" end
-            characterize!(det, imp_model, Vdep + 500, env = env, check_for_depletion = false, verbose = verbose, refinement_limits = refinement_limits)
+            sim = characterize!(det, imp_model, Vdep + 500, env = env, check_for_depletion = false, verbose = verbose, refinement_limits = refinement_limits)
         end
     else
         initialize_design!(det)
@@ -118,6 +118,7 @@ function characterize!(det::DetectorDesign{T}, imp_model::AbstractImpurityDensit
     else
         initialize_design!(det)
     end
+    sim
 end
 
 function characterize!(det::DetectorDesign{T}, imp_model::AbstractImpurityDensity{T}, reference_simulation::Simulation{T}; 
@@ -153,3 +154,89 @@ function characterize!(det::DetectorDesign{T}, imp_model::AbstractImpurityDensit
     end
     reference_simulation
 end
+
+#### OLD METHODS 
+#=
+function characterize!(det::DetectorDesign{T}, imp_model::AbstractImpurityDensity{T};
+        env::HPGeEnvironment = HPGeEnvironment(),
+        verbose::Bool = false, 
+        Vop::Real = default_operational_V, 
+        refinement_limits::Vector{<:Real} = [0.2, 0.1, 0.05, 0.02]
+    ) where {T<:AbstractFloat}
+    
+    det.Vop = Vop
+    det.Emin = missing
+    det.Emin_pos = missing
+    det.Vdep = missing
+    sim = Simulation{T}(det, imp_model, env)
+    calculate_electric_potential!(sim, 
+        refinement_limits = refinement_limits[1:min(2,end)], 
+        depletion_handling = true, 
+        verbose = verbose)
+    det.is_simulated = true
+    
+    if is_depleted(sim.point_types)
+        if verbose @info "Detector depletes under $(sim.detector.contacts[2].potential) V" end
+        if length(refinement_limits) > 2
+            if verbose @info "Refining Grid ...." end
+            calculate_electric_potential!(sim, 
+                refinement_limits = refinement_limits[3:end], 
+                depletion_handling = true, 
+                verbose = verbose, 
+                initialize = false)
+        end
+        if length(refinement_limits) <= 2 || is_depleted(sim.point_types)
+            Vdep = T(to_internal_units(estimate_depletion_voltage(sim, check_for_depletion = false, verbose = verbose)))
+            det.Vdep = Vdep
+            det.Vop = Vdep + 500
+            sim = Simulation{T}(det, imp_model, env)
+            if verbose @info "Simulating at Vdep + 500V...\n" end
+            calculate_electric_potential!(sim, 
+                refinement_limits = refinement_limits, 
+                depletion_handling = true, 
+                verbose = verbose)
+            calculate_electric_field!(sim, n_points_in_φ = 2);
+            if verbose @info "Looking for minimum electric field in the bulk" end
+            emin, pos = find_minimum_Efield_in_bulk(sim, require_local_min = true)
+            det.Emin = T(to_internal_units(emin))
+            det.Emin_pos = (T(to_internal_units(pos[1])), T(to_internal_units(pos[3])))
+            if verbose @info "$(Int(round(det.Emin, digits = 0))*internal_efield_unit) @ r = $(round(1.0*det.Emin_pos[1], digits = 1)*internal_length_unit), z = $(round(1.0*det.Emin_pos[2], digits = 1)*internal_length_unit)" end
+        end
+    end
+    sim
+end
+=#
+#=
+function characterize!(det::DetectorDesign{T}, imp_model::AbstractImpurityDensity{T}, reference_simulation::Simulation{T}; 
+    Vop::Real = reference_simulation.detector.contacts[2].potential,
+    verbose::Bool = false
+) where {T<:AbstractFloat}
+
+    det.Vop = Vop
+    det.Emin = missing
+    det.Emin_pos = missing
+    det.Vdep = missing
+    reference_simulation.detector = SolidStateDetector{T}(det, imp_model)
+    
+    update_till_convergence!( reference_simulation, ElectricPotential,
+                                    n_iterations_between_checks = 1000,
+                                    max_n_iterations = 50000,
+                                    depletion_handling = true,
+                                    verbose = verbose
+                            )
+    mark_bulk_bits!(reference_simulation.point_types.data)
+    mark_undep_bits!(reference_simulation.point_types.data, reference_simulation.imp_scale.data)
+    
+    if is_depleted(reference_simulation.point_types)
+        Vdep = T(to_internal_units(estimate_depletion_voltage(reference_simulation, check_for_depletion = false, verbose = verbose)))
+        det.Vdep = Vdep
+        calculate_electric_field!(reference_simulation, n_points_in_φ = 2);
+        if verbose @info "Looking for minimum electric field in the bulk" end
+        emin, pos = find_minimum_Efield_in_bulk(reference_simulation, require_local_min = true)
+        det.Emin = T(to_internal_units(emin))
+        det.Emin_pos = (T(to_internal_units(pos[1])), T(to_internal_units(pos[3])))
+        if verbose @info "$(Int(round(det.Emin, digits = 0))*internal_efield_unit) @ r = $(round(1.0*det.Emin_pos[1], digits = 1)*internal_length_unit), z = $(round(1.0*det.Emin_pos[2], digits = 1)*internal_length_unit)" end
+    end
+    reference_simulation
+end
+=#
